1e7dc823b3679f4c2f063ff86feada91edc5dfc0
Adam <f820920@gmail.com>
2015-05-14 12:38:17 +0800

    model>signup_model fail

--- excellent/application/controllers/signup.php
+++ excellent/application/controllers/signup.php
@@ -36,32 +36,47 @@ class Signup extends CI_Controller {
 
 	public function ajax_create_signup()
 	{
-		$s_id = $this->input->post('s_id');
-		if ($s_id) {
-			echo json_encode(array('status' => true, 'msg' => '成功!'));
+		/*$datas=array(
+			$s_id => $this->input->post('s_id'),		
+			$name => $this->input->post('name'),
+			$department => $this->input->post('department'),
+			$sex => $this->input->post('sex'),
+			$ps => $this->input->post('ps')
+		);*/
+			$s_id = $this->input->post('s_id');		
+			$name = $this->input->post('name');
+			$department = $this->input->post('department');
+			$sex = $this->input->post('sex');
+			$ps = $this->input->post('ps');
+		
+		if (!$s_id) {
+			echo json_encode(array('status' => false, 'msg' => '失敗'));
+		} else if($name==''){
+			echo json_encode(array('status' => false, 'msg' => '請輸入姓名!'));
+		} else if ($department=="0") {
+			echo json_encode(array('status' => false, 'msg' => '請選擇單位!'));
+		} else if ($sex=="0"){
+			echo json_encode(array('status' => false, 'msg' => '請選擇性別!'));
 		} else {
-			echo json_encode(array('status' => false, 'msg' => '失敗!'));
+			$this->signup_model->create_signup();
+			echo json_encode(array('status' => true, 'msg' => '報名成功!'));
 		}
 		//$this->load->helper('url');
-		// foreach ($_POST as $key => $data) {
-		// 	if (!$data && $key != 'ps') {
-		// 		echo json_encode(array('status' => false, 'msg' => '請確認欄位是否填妥!'));
-				
-		// 		echo "<script>alert('NO');</script>";
-		// 		die();
-		// 	}
-		// }	
-		
-		// echo "<script>alert('SUCCESS');</script>";
-		// $this->signup_model->create_signup();
-		// echo json_encode(array('status' => true, 'msg' => '報名成功'));
-		// $data['signup'] = $this->signup_model->get_signup();
-		// $data['title'] = 'News archive';
-		// $this->load->view('templates/header', $data);
-		// $this->load->view('templates/left_aside', $data);
-		// $this->load->view('signup/signuplist', $data);
-		// $this->load->view('templates/right_aside', $data);
-		// $this->load->view('templates/footer');
+		/*foreach ($datas as $key => $data) {
+			if (!$data && $key != 'ps') {
+				echo json_encode(array('status' => false, 'msg' => '請確認欄位是否填妥!'));
+				die();
+			}
+		}	*/
+		//$this->signup_model->create_signup();
+		//echo json_encode(array('status' => true, 'msg' => '報名成功'));
+		/*$data['signup'] = $this->signup_model->get_signup();
+		$data['title'] = 'News archive';
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/left_aside', $data);
+		$this->load->view('signup/signuplist', $data);
+		$this->load->view('templates/right_aside', $data);
+		$this->load->view('templates/footer');*/
 
 	}
 
