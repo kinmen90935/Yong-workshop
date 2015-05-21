cafc606f7ca37f76dca467f704c7026cb45c00d8
Adam <f820920@gmail.com>
2015-05-21 22:21:54 +0800

    CHANGED 2

--- excellent/application/views/templates/header.php
+++ excellent/application/views/templates/header.php
@@ -38,7 +38,7 @@
 <body style="height:90%;">
 	<div id="headerContainer">
 	    <div id="header" class="group">
-	    	<a id="logo" href="<?=base_url();?>home"><img src="<?=base_url();?>assets/images/logo.png" alt="" width="10%"></a>
+	    	<a id="logo" href="<?=base_url();?>news"><img src="<?=base_url();?>assets/images/logo.png" alt="" width="10%"></a>
 	        <div id="headerArea">
 	            <a href="http://www.ntue.edu.tw" target="_blank" ><span class="ui-icon  ui-icon-home"></span>北教大首頁</a>
 	            <a href="http://ctld.ntue.edu.tw/" target="_blank" ><span class="ui-icon  ui-icon-home"></span>教學發展中心首頁</a>
