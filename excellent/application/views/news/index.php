d18e799f2c70cc3c83d9b9b4e04a7d125032083d
peiju <wholly4lucky@gmail.com>
2015-05-25 18:03:09 +0800

    test

--- excellent/application/views/news/index.php
+++ excellent/application/views/news/index.php
@@ -1,6 +1,53 @@
 <?php
 	$this->load->view('templates/header');
 ?>
+<script>
+	console.log("sc");
+	function ajax_news_list (page) {
+		if(page==null)
+			page=1;
+		else
+			page = page;
+		console.log("function_page");
+		$.ajax({
+	            url : "news/ajax_news_list",
+	            type: "POST",
+	            data : {'page' : page},
+	            dataType: 'json',
+	            success:function(rtn, textStatus, jqXHR) {
+	              if (rtn.status) {
+	                $('#news').html(rtn.news);
+					$('#pg').html(rtn.page);
+	                console.log("if");
+	                console.log(page);
+
+	              } else {
+	              	console.log("else");
+	              	console.log(jqXHR.responseText);
+	              }
+	            },
+	            error: function(jqXHR, textStatus, errorThrown) {
+	              console.log(jqXHR.responseText);
+	            }
+	          });
+	}
+
+	$(document).ready(function () {
+		ajax_news_list();
+
+		$('body').on('click','.pagination li',function(event) {
+			event.preventDefault();
+
+			$('.pagination li').find('a').removeClass('active');
+			$(this).find('a').addClass('active');
+			
+			var a = $(this).data('page');
+			console.log($(this).data('page'));
+			ajax_news_list(a);
+		})
+	});
+</script>
+<title><?php echo $title;?></title>
 <div class="top_slider">
 	<ul id="moodular">			
 		<li><img src="<?= base_url('/assets/images/image_3.jpg');?>">123</li>
@@ -11,21 +58,18 @@
 
 <div class="inner">
 	<div class="col span_8_of_12">
-	    <p style="border-bottom-style:solid; border-width:medium; color:#4A67FF;">
-	      首頁
-	    </p>
-		<div>
-			news
+		<div class="col span_12_of_12">
+		    <p style="border-bottom-style:solid; border-width:medium; color:#4A67FF;">
+		      首頁
+		    </p>
 		</div>
-	 	<div class="col span_2_of_12"></div>
-	 	<div class="col span_9_of_12">
-		<div>
+		<div id="news">
+			news
+		</div>	
+		<div id="pg" data-role="page">
 			pg
 		</div>
-		</div>
 	</div>
-
-
 	<div class="col span_4_of_12">
 		<p style="border-bottom-style:solid; border-width:medium; color:#4A67FF;">
 		  <span class="ui-icon ui-icon-play" ></span>
@@ -34,23 +78,23 @@
 		<div class="block">
 	        <p>職涯百工圖短片提供了各系所未來職涯發展的簡介，以訪問畢業學長姐及系所介紹的方式，用輕鬆易懂又深入的短片，引領同學瞭解各系所的出路，提供同學畢業後的方向指引。</p>
 	        <style type="text/css">
-	        .video-container {
-	            position: relative;
-	            padding-bottom: 56.25%;
-	            padding-top: 30px; height: 0; overflow: hidden;
-	        }
-	        .video-container iframe,
-	        .video-container object,
-	        .video-container embed {
-	            position: absolute;
-	            top: 0;
-	            left: 0;
-	            width: 100%;
-	            height: 100%;
-	        }
-	        .fb-like-box, .fb-like-box span, .fb-like-box.fb_iframe_widget span iframe {
-	            width: 100% !important;
-	        }
+		        .video-container {
+		            position: relative;
+		            padding-bottom: 56.25%;
+		            padding-top: 30px; height: 0; overflow: hidden;
+		        }
+		        .video-container iframe,
+		        .video-container object,
+		        .video-container embed {
+		            position: absolute;
+		            top: 0;
+		            left: 0;
+		            width: 100%;
+		            height: 100%;
+		        }
+		        .fb-like-box, .fb-like-box span, .fb-like-box.fb_iframe_widget span iframe {
+		            width: 100% !important;
+		        }
 	        </style>
 	        <div class="video-container">
 	            <iframe src="http://www.youtube.com/embed/wuSV9uPl_UY?rel=0" frameborder="0" width="380" height="214"></iframe>
@@ -79,25 +123,6 @@
 	$this->load->view('templates/footer');
 ?>
 
-<script>
-function ajax_news_list (page) {
-	page = page || 1;
-
-	$.post("ajax_news_list",{page : page},function(rtn){
-		$(news).html(rtn.news);
-		$(pg).html(rtn.page);
-	})
-}
-
-$(document).ready(function () {
-	ajax_news_list();
-
-	$('body').on(click,'page',function(argument) {
-		var a = page-data;
-		ajax_news_list(a);
-	})
-});
-</script>
 <style>
 	#page_nav
 	{
@@ -199,3 +224,4 @@ $(document).ready(function () {
 	  padding: 4px;
 	}
 </style>
+
